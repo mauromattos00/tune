757d6c990a08759ddf604889c0ee3b3e0138d133
Mauro Mattos <mauromattos00@gmail.com>
2021-09-27 23:12:20 -0300

    test: fix initial unit tests

--- apps/tune/src/app/video-player/video-player.component.spec.ts
+++ apps/tune/src/app/video-player/video-player.component.spec.ts
@@ -1,22 +1,18 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
 import { VideoPlayerComponent } from './video-player.component';
 
 describe('VideoPlayerComponent', () => {
   let component: VideoPlayerComponent;
-  let fixture: ComponentFixture<VideoPlayerComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ VideoPlayerComponent ]
-    })
-    .compileComponents();
-  });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(VideoPlayerComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    TestBed.configureTestingModule({
+      providers: [
+        VideoPlayerComponent
+      ]
+    });
+
+    component = TestBed.inject(VideoPlayerComponent);
   });
 
   it('should create', () => {
