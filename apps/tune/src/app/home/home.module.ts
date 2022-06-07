d6caf501f37d31c855ac177ffccfbe4e9945fb45
Mauro Mattos <mauromattos00@gmail.com>
2022-06-07 16:15:27 -0300

    refactor: remove unnecessary component

--- apps/tune/src/app/home/home.module.ts
+++ apps/tune/src/app/home/home.module.ts
@@ -3,12 +3,11 @@ import { NgModule } from '@angular/core';
 import { VideoPlayerModule } from '@tune/video-player';
 import { TuneUiModule } from '@tune/tune-ui';
 
-import { HeadingsComponent } from './components/headings/headings.component';
 import { HomeComponent } from './home.component';
 
 @NgModule({
   imports: [VideoPlayerModule, TuneUiModule],
-  declarations: [HomeComponent, HeadingsComponent],
+  declarations: [HomeComponent],
   exports: [HomeComponent],
 })
 export class HomeModule {}
