21ead1c4df99e1500447ee93b72fd34cae21ff2d
Mauro Mattos <mauromattos00@gmail.com>
2022-06-07 18:05:59 -0300

    feat: implement ngrx effects

--- libs/tune-state/src/lib/tune-state.module.ts
+++ libs/tune-state/src/lib/tune-state.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   imports: [
     CommonModule,
     StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
     StoreDevtoolsModule.instrument(),
   ],
 })
