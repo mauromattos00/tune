4b0035e472e52eb17328d2ba4678c12bdcfa7e7b
Mauro Mattos <mauromattos00@gmail.com>
2022-06-07 17:50:22 -0300

    feat: install @ngrx/store

--- apps/tune/src/app/app.module.ts
+++ apps/tune/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
 
 import { VideoPlayerModule } from '@tune/video-player';
 
@@ -16,6 +17,7 @@ import { HomeModule } from './home/home.module';
     VideoPlayerModule,
     AppRoutingModule,
     HomeModule,
+    StoreModule.forRoot({}, {}),
   ],
   bootstrap: [AppComponent],
 })
